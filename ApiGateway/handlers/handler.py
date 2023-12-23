ff815d35403dab074e32c63fe5753f9e7b9a6dc4
conan194351 <phu.nl194351@sis.hust.edu.vn>
2023-12-23 23:25:45 +0700

    update: put webhook mesage

--- ApiGateway/handlers/handler.py
+++ ApiGateway/handlers/handler.py
@@ -1,6 +1,11 @@
 # handler.py
 from flask import jsonify,request
-from services.service import HealthCheckService
+from services.service import HealthCheckService,Service
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from the .env file
+load_dotenv()
 
 class HealthCheckHandler:
     @staticmethod
@@ -9,7 +14,31 @@ class HealthCheckHandler:
         health_status = HealthCheckService.check_health()
         # Trả về response JSON
         return jsonify(health_status)
+    
+class Handler:
+    @staticmethod
     def chat():
         data = request.get_json()
         msg_value = data.get('msg')
         return jsonify(HealthCheckService.chatbot(msg_value))
+    def webhook():
+        data = request.get_json()
+        if data.get('object') == "page":
+            entries = data.get('entry')
+            Service.webhook(entries)
+        else:
+            return jsonify(404)
+        
+    def get_webhook():
+        token = os.getenv('MYTOKEN', None)
+        print(token)
+        mode = request.args.get("hub.mode")
+        verify_token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if mode and verify_token:
+            if mode == "subscribe" and token == verify_token:
+                print("WEBHOOK_VERIFIED")
+                return challenge, 200
+            else:
+                return "Forbidden", 403
+            
--- ApiGateway/main.py
+++ ApiGateway/main.py
@@ -1,11 +1,11 @@
 # app.py
 from flask import Flask
-from routers.route import health_check_bp
+from routers.route import routers
 
 app = Flask(__name__)
 
 # Đăng ký Blueprint cho route health check
-app.register_blueprint(health_check_bp)
+app.register_blueprint(routers)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=80)
+    app.run(debug=True,port=8081)
