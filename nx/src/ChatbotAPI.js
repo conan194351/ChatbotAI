d865ec1b6b3fcd8e4eed1f095d20c6bce3175640
Sotatek-PhuNguyen2 <phu.nguyen2@sotatek.com>
2024-01-05 09:08:07 +0700

    add ui for chatbot

--- nx/src/ChatbotAPI.js
+++ nx/src/ChatbotAPI.js
@@ -1,12 +1,47 @@
 const API = {
-  GetChatbotResponse: async message => {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        if (message === "hi") resolve("Welcome to chatbot!");
-        else resolve("echo : " + message);
-      }, 2000);
+  GetChatbotResponse: async (message) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        console.log('message:', message);
+
+        if (message === 'hi') {
+          resolve('Welcome to chatbot!');
+        } else {
+          console.log('Making API request with message:', message);
+
+          // Thiết lập cấu hình cho fetch
+          const requestOptions = {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ msg: message }),
+          };
+
+          // Sử dụng fetch để gọi API
+          const response = await fetch('http://localhost:8081/chat', requestOptions);
+
+          // Kiểm tra trạng thái của response
+          if (!response.ok) {
+            throw new Error('Không thể kết nối đến API');
+          }
+
+          // Chuyển đổi response thành dạng JSON
+          const responseData = await response.json();
+
+          console.log('API response:', responseData);
+
+          // Lấy dữ liệu từ trường 'Sam' trong response
+          const chatbotResponse = responseData.Sam;
+
+          resolve(chatbotResponse);
+        }
+      } catch (error) {
+        console.error('Error making API request:', error);
+        reject('Error making API request');
+      }
     });
-  }
+  },
 };
 
 export default API;
